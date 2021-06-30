94b197879fc4d346deff47c6d3852f93b767268f
Matthew Sanchez <MSanchez@tql.com>
2021-06-30 16:04:22 -0400

    request commit

--- src/app/request/request-create/request-create.component.ts
+++ src/app/request/request-create/request-create.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SystemService } from 'src/app/core/system.service';
+import { Request } from '../request.class';
+import { RequestService } from '../request.service';
 
 @Component({
   selector: 'app-request-create',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestCreateComponent implements OnInit {
 
-  constructor() { }
+  
+  request: Request = new Request();
+  requests: Request[] = [];
+
+  constructor(
+    private syssvc: SystemService,
+    private requestsvc: RequestService,
+    private router: Router
+
+  ) { }
+
+  save(): void {
+    this.request.id = +this.request.id;
+    console.debug("B4", this.request);
+    this.requestsvc.create(this.request).subscribe(
+      res => {
+        console.log("Create successful"); this.router.navigateByUrl("request/list")
+      },
+      err => {
+        console.error(err);
+      });
+
+  }
 
   ngOnInit(): void {
+    this.requestsvc.list().subscribe(
+      res => { console.log(res); this.requests = res; },
+      err => { console.error(err); }
+    )
   }
 
 }
--- src/app/request/request-edit/request-edit.component.ts
+++ src/app/request/request-edit/request-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RequestService } from '../request.service';
+import { Request } from '../request.class';
 
 @Component({
   selector: 'app-request-edit',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./request-edit.component.css']
 })
 export class RequestEditComponent implements OnInit {
+  
+  request!: Request;
+  id: number = 0;
 
-  constructor() { }
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private requestsvc: RequestService
+
+  ) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params.id
+    this.requestsvc.get(this.id).subscribe(
+      res => { console.log(res); this.request = res; },
+      err => { console.error(err) }
+    )
+  }
+
+  save(): void {
+    this.request.id = +this.request.id;
+    console.debug("B4", this.request);
+    this.requestsvc.change(this.request).subscribe(
+      res => { console.log("Create successful"); this.router.navigateByUrl("request/list"); },
+      err => { console.error(err) });
+    ;
   }
 
 }
